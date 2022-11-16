a20b01fe1a434da65f952665e86c7edd5003b836
tmgasek <tomaszgasek28@gmail.com>
2022-11-16 21:28:23 +0000

    skeleton complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO");
+});
+
+app.listen(port, () => {
+  console.log(`server running at localhost:${port}`);
+});
